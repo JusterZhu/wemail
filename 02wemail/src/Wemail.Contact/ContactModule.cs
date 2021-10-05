6c9d52523e7850de40061292b4cb1f2f16212332
JusterZhu <zhuzhen723723@163.com>
2021-10-05 20:27:30 +0800

    wemail02

---
+++ 02wemail/src/Wemail.Contact/ContactModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Wemail.Contact.Views;
+
+namespace Wemail.Contact
+{
+    public class ContactModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
